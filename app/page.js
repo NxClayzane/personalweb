55f4846d5e43710a32311879aa56ccefc7c417a4
Clayzane <66798211+NxClayzane@users.noreply.github.com>
2023-10-19 20:23:23 +0400

    test

---
+++ app/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="box">
+        <button className="cybr-btn">
+        こんにちは<span aria-hidden>_</span>
+          <span aria-hidden className="cybr-btn__glitch">
+          こんにちは
+          </span>
+          <span aria-hidden className="cybr-btn__tag">
+            Nexus-12
+          </span>
+        </button>
+      </div>
+    </main>
+  );
+}
